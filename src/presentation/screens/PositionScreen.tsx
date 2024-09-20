b7919f165a07fd4d5784c3a37dceb65dbdd842df
martinrdrz <martinrdrz@hotmail.com>
2024-09-20 20:38:07 -0300

    trabajando sobre el diseño

--- src/presentation/screens/PositionScreen.tsx
+++ src/presentation/screens/PositionScreen.tsx
@@ -3,8 +3,10 @@ import {Text} from 'react-native-paper';
 
 export const PositionScreen = () => {
   return (
-    <View>
-      <Text>PositionScreen</Text>
+    <View style={styles.container}>
+      <View style={styles.greenBox} />
+      <View style={styles.purpleBox} />
+      <View style={styles.orangeBox} />
     </View>
   );
 };
@@ -13,15 +15,37 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#28C4D9',
-  },
-  title: {
-    fontSize: 20,
-    borderWidth: 10,
-    paddingHorizontal: 30,
-    paddingVertical: 10,
+    //justifyContent: 'center',
+    //alignItems: 'center',
   },
   purpleBox: {
-    height: 50,
+    width: 100,
+    height: 100,
     backgroundColor: 'purple',
+    borderWidth: 10,
+    borderColor: 'white',
+    position: 'absolute',
+    top: 0,
+    right: 0,
+  },
+  orangeBox: {
+    width: 100,
+    height: 100,
+    backgroundColor: '#F0A23B',
+    borderWidth: 10,
+    borderColor: 'white',
+  },
+  greenBox: {
+    //width: 100,
+    //height: 100,
+    backgroundColor: 'green',
+    borderWidth: 10,
+    borderColor: 'white',
+    position: 'absolute',
+    // bottom: 0,
+    // right: 0,
+    // top: 0,
+    // left: 0,
+    ...StyleSheet.absoluteFillObject,
   },
 });
